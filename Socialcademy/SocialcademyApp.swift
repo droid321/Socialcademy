bdd5e04c09d7cdf42709e17a9fca92d498338a95
Carl <cgpsanagustin@yahoo.com>
2025-12-06 18:43:42 +1100

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Carl SanAgustin on 6/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
