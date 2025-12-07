3953c5ae5460c55e4eee8930a1225b40488784e2
Carl <cgpsanagustin@yahoo.com>
2025-12-08 01:12:51 +1100

    Testing connecting firebase to app
    This is a test practice project on connecting firebase database to the app using a tutorial

---
+++ Socialcademy/Models/Post.swift
@@ -0,0 +1,33 @@
+//
+//  Post.swift
+//  Socialcademy
+//
+//  Created by Carl SanAgustin on 6/12/2025.
+//
+
+import Foundation
+
+
+struct Post: Identifiable, Codable {
+    var title: String
+    var content: String
+    var authorName: String
+    var timestamp = Date()
+    var id = UUID()
+    
+    func contains (_ string: String) -> Bool {
+        let properties = [title, content, authorName].map { $0.lowercased() }
+        let query = string.lowercased()
+        
+        let matches = properties.filter { $0.contains(query) }
+        return !matches.isEmpty
+    }
+}
+
+extension Post {
+    static let testPost = Post (
+        title: "Lorem ipsum",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        authorName: "Carl SanAgustin"
+    )
+}
---
+++ Socialcademy/View Models/PostsViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  PostsViewModel.swift
+//  Socialcademy
+//
+//  Created by Carl SanAgustin on 7/12/2025.
+//
+
+import Foundation
+
+@MainActor
+class PostsViewModel: ObservableObject {
+    @Published var posts = [Post.testPost]
+    
+    func makeCreateAction() -> NewPostForm.CreateAction {
+        return { [weak self] post in
+            try await PostsRepository.create(post)
+            self?.posts.insert(post,at: 0)
+        }
+    }
+    
+}
